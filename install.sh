b6c0ec4dee2564dffc5a8ba42b1d337dbe06b845
rafallltm <rafallltm@gmail.com>
2023-12-13 21:20:09 -0300

    script de instalacao da ide vscode no linux debian usando wget

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Baixa a chave GPG do repositório da Microsoft
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+
+# Adiciona o repositório do VS Code
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+
+# Atualiza a lista de pacotes
+sudo apt update
+
+# Instala o VS Code
+sudo apt install code
